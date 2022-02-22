25fcd5563e86960e583c64f9712c2923967ad2c8
Your Name <you@example.com>
2022-02-22 08:54:58 +0100

    init

---
+++ src/main/java/com/lechos22j/hotelsapplicationtest/HotelsApplicationTestApplication.java
@@ -0,0 +1,13 @@
+package com.lechos22j.hotelsapplicationtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelsApplicationTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelsApplicationTestApplication.class, args);
+    }
+
+}
